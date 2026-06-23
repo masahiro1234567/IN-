b5154a69a5f29503ed643d083908f0f3a3559ade
masahiro1234567 <masa.11250212@gmail.com>
2026-06-23 13:23:17 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import INDashboard from "./INDashboard";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <INDashboard />
+  </React.StrictMode>
+);
